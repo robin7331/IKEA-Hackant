37e3fd896edb02d277275069bbc93064d1d66547
Stefan Kinzel <stefan@kinzel.net>
2017-02-20 18:06:02 +0100

    Use internal pullup-resistors instead of external pulldown

--- src/main.cpp
+++ src/main.cpp
@@ -19,7 +19,6 @@
 #include "system_clock.h"
 #include <EEPROM.h>
 
-
 uint16_t lastPosition = 0;
 uint16_t memOne = 0;
 uint16_t memTwo = 0;
@@ -33,9 +32,9 @@ uint8_t currentTableMovement = 0;
 const int moveTableUpPin = PD4;
 const int moveTableDownPin = PD7;
 
-const int moveUpButton = PD3;
-const int moveM2Button = PD6;
-const int moveM1Button = PD5;
+const int moveUpButton = 3;
+const int moveM2Button = 9;
+const int moveM1Button = 5;
 const int moveDownButton = 8;
 
 int pressedButton = 0;
@@ -184,7 +183,7 @@ void processLINFrame(LinFrame frame) {
 
 void readButtons() {
 
-  if (digitalRead(moveUpButton) == HIGH) {
+  if (!digitalRead(moveUpButton)) {
 
     pressedButton = moveUpButton;
     if (lastPressedButton != pressedButton) {
@@ -194,7 +193,7 @@ void readButtons() {
     return;
   }
 
-  if (digitalRead(moveM1Button) == HIGH) {
+  if (!digitalRead(moveM1Button)) {
     pressedButton = moveM1Button;
     if (lastPressedButton != pressedButton) {
       Serial.println("Button M1 Pressed");
@@ -204,7 +203,7 @@ void readButtons() {
     return;
   }
 
-  if (digitalRead(moveM2Button) == HIGH) {
+  if (!digitalRead(moveM2Button)) {
     pressedButton = moveM2Button;
     if (lastPressedButton != pressedButton) {
       Serial.println("Button M2 Pressed");
@@ -214,7 +213,7 @@ void readButtons() {
     return;
   }
 
-  if (digitalRead(moveDownButton) == HIGH) {
+  if (!digitalRead(moveDownButton)) {
     pressedButton = moveDownButton;
     if (lastPressedButton != pressedButton) {
       Serial.println("Button DN Pressed");
@@ -298,10 +297,10 @@ void setup() {
   pinMode(moveTableUpPin, OUTPUT);
   pinMode(moveTableDownPin, OUTPUT);
 
-  pinMode(moveUpButton, INPUT);
-  pinMode(moveDownButton, INPUT);
-  pinMode(moveM1Button, INPUT);
-  pinMode(moveM2Button, INPUT);
+  pinMode(moveUpButton, INPUT_PULLUP);
+  pinMode(moveDownButton, INPUT_PULLUP);
+  pinMode(moveM1Button, INPUT_PULLUP);
+  pinMode(moveM2Button, INPUT_PULLUP);
 
   Serial.print("moveUpButton ");
   Serial.println(moveUpButton);
